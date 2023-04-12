150a9ad244e308cf559670b628e49d76d9db05b5
Ayesha <ayesha18mbstu@gmail.com>
2023-04-12 13:20:37 +0600

    404 added

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-import React, { children } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Main from './components/Layout/Main';
-import Home from './components/Home/Home';
-import Statistics from './components/Statistics/Statistics';
-import AppliedJobs from './components/AppliedJobs/AppliedJobs';
-import Blog from './components/Blog/Blog';
-import JobDetails from './components/jobDetails/JobDetails';
+import React, { children } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/Layout/Main";
+import Home from "./components/Home/Home";
+import Statistics from "./components/Statistics/Statistics";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
+import Blog from "./components/Blog/Blog";
+import JobDetails from "./components/jobDetails/JobDetails";
 
 const router = createBrowserRouter([
   {
@@ -19,33 +16,44 @@ const router = createBrowserRouter([
     element: <Main></Main>,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home></Home>,
-        loader: () => fetch('categories.json'),
+        loader: () => fetch("categories.json"),
       },
       {
-        path: 'detail/:id',
+        path: "detail/:id",
         element: <JobDetails></JobDetails>,
-        loader: () => fetch('/featuredJobs.json')
+        loader: () => fetch("/featuredJobs.json"),
       },
       {
-        path: 'statistics',
+        path: "statistics",
         element: <Statistics></Statistics>,
       },
       {
-        path: 'applied-jobs',
-        element: <AppliedJobs></AppliedJobs>
+        path: "applied-jobs",
+        element: <AppliedJobs></AppliedJobs>,
       },
       {
-        path: 'blog',
-        element: <Blog></Blog>
+        path: "blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "*",
+        element: (
+          <div>
+            <h1 className="font-bold text-3xl text-red-700">404</h1>
+            <p className="font-semibold">
+              Sorry, an unexpected error has occurred!
+            </p>
+          </div>
+        ),
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
