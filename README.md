5b95485dcb7e53e7d6bc3923bfa58402886632b0
Ayesha <ayesha18mbstu@gmail.com>
2023-04-14 23:39:00 +0600

    readme file info added

--- README.md
+++ README.md
@@ -6,10 +6,14 @@
 
 * useState, useEffect help to declare a state more efficiently.
 
+* recharts helps to make an area chart to show data statistically.
+
+* cartData is a custom loader which can be used in multiple files.
 
 
 
-## live link: https://cute-cobbler-f12fc2.netlify.app/
+
+[live link:](https://cute-cobbler-f12fc2.netlify.app/)
 
 
 
--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -4,18 +4,19 @@ import AppliedJob from "../AppliedJob/AppliedJob";
 
 const AppliedJobs = () => {
   const { products } = useLoaderData();
-
   const { initialCart } = useLoaderData();
   console.log(initialCart);
 
   return (
     <div>
-      <h1 className="font-semibold text-2xl mb-10">
-        Applied Jobs {initialCart.length}
-      </h1>
+      <h1 className="font-semibold text-2xl mb-10">Applied Jobs</h1>
+      <h2 className="text-xl mb-10 text-zinc-600 underline">
+        {initialCart.length ? "Review Applied Jobs" : "Review is Empty"}
+      </h2>
+
       <div className="min-h-screen items-start text-gray-900">
         {initialCart.map((item) => (
-          <AppliedJob key={item.id} item={item} />
+          <AppliedJob key={item.i} item={item} />
         ))}
       </div>
     </div>
